9d72a3d32f20e654af979fc1acc8cf1cd9aaa74b
Andrea Cuevas <andicuevasp@gmail.com>
2022-12-05 13:25:13 +0100

    wip added desktop styles

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -2,10 +2,14 @@ export default function Results(props) {
 
     return (
         <div className="results">
-          <h3>Tip Amount <br/> <span className="person"> / person</span></h3>
-          <span className="amount">€{props.tipAmountPerPerson}</span>
-          <h3>Total Amount <br/> <span className="person"> / person</span></h3>
-          <span className="amount">€{props.totalAmountPerPerson}</span>
+          <div className="totals">
+            <h3>Tip Amount <br/> <span className="person"> / person</span></h3>
+            <span className="amount">€{props.tipAmountPerPerson}</span>
+          </div>
+          <div className="totals">
+            <h3>Total Amount <br/> <span className="person"> / person</span></h3>
+            <span className="amount">€{props.totalAmountPerPerson}</span>
+          </div>
           <button onClick={props.reset}>Reset</button>
         </div>
     )
